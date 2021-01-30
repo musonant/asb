0b75ae289a310f529fae32f6517c865d0833c347
Emmanuel Osuh <mrmusonant@gmail.com>
2021-01-30 09:50:45 +0100

    Add breadcrumb

--- src/pages/Division/index.jsx
+++ src/pages/Division/index.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './styles.scss';
 import Header from '../../components/Header';
+import BreadCrumb from '../../components/Breadcrumb';
 
 const Division = () => {
+  const breadCrumbTree = [
+    {
+      label: 'Divisions',
+      path: '/',
+    },
+    {
+      label: 'Module',
+      path: '/module',
+    },
+  ];
+
   return (
-    <Header />
+    <>
+      <Header />
+      <div className="page-wrapper">
+        <BreadCrumb tree={breadCrumbTree} />
+      </div>
+    </>
   );
 };
 
