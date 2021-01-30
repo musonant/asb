7cd91f7d7c58cb905f6d7243d9c5e1304a66a6fc
Emmanuel Osuh <mrmusonant@gmail.com>
2021-01-30 07:09:06 +0100

    Setup development environment and boilerplate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      ASB Frontend Engineering Test
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NumberSummaryCard/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NumberSummaryCard = () => {
+  return (
+    <h1>NumberSummaryCard</h1>
+  )
+};
+
+export default NumberSummaryCard;
